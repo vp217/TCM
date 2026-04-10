ae2482caa7e6345ce37038de85d961a3ca31716d
vp217 <vikas.prajapati@aniruddhagps.com>
2026-04-10 21:51:40 +0530

    added CI pipeline

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,15 @@
-# def test_google_title(page):
-#     page.goto("https://www.google.com")
-#     assert "Google" in page.title()
+def test_google_title(page):
+    page.goto("https://www.google.com")
+    assert "Google" in page.title()
 
-import pytest
+# import pytest
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     rep = outcome.get_result()
 
-    if rep.when == "call" and rep.failed:
-        page = item.funcargs.get("page")
-        if page:
-            page.screenshot(path="failure.png")
+#     if rep.when == "call" and rep.failed:
+#         page = item.funcargs.get("page")
+#         if page:
+#             page.screenshot(path="failure.png")
