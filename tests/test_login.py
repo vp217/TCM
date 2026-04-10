3bf853a5d2ec289394539fa1c03f6c2e9a001836
vp217 <vikas.prajapati@aniruddhagps.com>
2026-04-10 22:22:12 +0530

    added CI pipeline

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,14 +2,6 @@ def test_google_title(page):
     page.goto("https://www.google.com")
     assert "Google" in page.title()
 
-# import pytest
 
-# @pytest.hookimpl(hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     outcome = yield
-#     rep = outcome.get_result()
-
-#     if rep.when == "call" and rep.failed:
-#         page = item.funcargs.get("page")
-#         if page:
-#             page.screenshot(path="failure.png")s
+def test_sample():
+    assert True
